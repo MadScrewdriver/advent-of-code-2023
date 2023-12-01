f326a1b3a89d56474f73bff8e4549168d1801af9
Franciszek Lajszczak <f.lajszczak@gmail.com>
2023-12-01 23:40:28 +0100

    Comments to asm added

--- day1a/extract.asm
+++ day1a/extract.asm
@@ -2,82 +2,99 @@ section .data use32
     save_first_number db 0
     save_second_number db 0
 
-
 section .text use32
 
-global	_extract
+global _extract
 
 _extract:
-	push ebp
+    push ebp
     mov ebp, esp
 
+    ; Initialize registers
     xor eax, eax
     xor ecx, ecx
 
-    mov ecx, [ebp+8]
-    mov eax, [ebp+12]
+    ; Load arguments into registers
+    mov ecx, [ebp+8]   ; Length of the array
+    mov eax, [ebp+12]  ; Pointer to the array
 
+    ; Initialize ebx for indexing and edx for temporary storage
     xor edx, edx
     xor ebx, ebx
 
+    ; Reset saved numbers to 0
     mov [save_first_number], edx
     mov [save_second_number], edx
 
-
     extract_loop:
+        ; Load the current character into dl
         xor edx, edx
         mov dl, [eax+ebx]
 
+        ; Check if character is a digit
         cmp dl, '0'
         jl continue
         cmp dl, '9'
         jg continue
+
+        ; If a digit is found, decide whether to save it as first or second number
         jle pass
 
         continue:
+            ; Increment the index and continue the loop
             add ebx, 1
             loop extract_loop
             jmp return
 
         pass:
+            ; Check if the first number is already saved
             mov dh, [save_first_number]
             cmp dh, 0
             je save_first
             jne save_second
 
             save_first:
+                ; Save the digit as the first number
                 sub dl, '0'
                 mov [save_first_number], dl
                 jmp continue
 
             save_second:
+                ; Save the digit as the second number
                 sub dl, '0'
                 mov [save_second_number], dl
                 jmp continue
 
-return:
-    xor edx, edx
-    mov dl, [save_first_number]
-    mov dh, [save_second_number]
+    return:
+        ; Prepare the result in edx
+        xor edx, edx
+        mov dl, [save_first_number]
+        mov dh, [save_second_number]
 
-    add dl, dl
-    add dl, dl
-    add dl, [save_first_number]
-    add dl, dl
-
-    cmp dh, 0
-    je double_first
-    jne combine_both
-
-    double_first:
+        ; Multiply the first number by 10
+        add dl, dl
+        add dl, dl
         add dl, [save_first_number]
-        jmp exit
-    combine_both:
-        add dl, [save_second_number]
-        jmp exit
+        add dl, dl
 
-exit:
-    xor dh, dh
-    mov eax, edx
-    leave
-    ret
+        ; Check if the second number exists and choose the next operation
+        cmp dh, 0
+        je double_first
+        jne combine_both
+
+        double_first:
+            ; Double the first number if the second number doesn't exist
+            add dl, [save_first_number]
+            jmp exit
+
+        combine_both:
+            ; Add the second number to the doubled first number
+            add dl, [save_second_number]
+            jmp exit
+
+    exit:
+        ; Set the result in eax (return register) and clean up
+        xor dh, dh
+        mov eax, edx
+        leave
+        ret
--- day1a/sum.asm
+++ day1a/sum.asm
@@ -1,31 +1,29 @@
-
 section .text use32
 
-global	_sum
+global _sum
 
 _sum:
-	push ebp
-    mov ebp, esp
+    push ebp           ; Save the base pointer
+    mov ebp, esp       ; Set base pointer for the current stack frame
 
-    xor eax, eax
-    xor ecx, ecx
+    xor eax, eax       ; Clear eax register
+    xor ecx, ecx       ; Clear ecx register
 
-    mov ecx, [ebp+8]
-    mov eax, [ebp+12]
+    mov ecx, [ebp+8]   ; Move the length of the array into ecx
+    mov eax, [ebp+12]  ; Move the pointer to the array into eax
 
-    xor edx, edx
-    xor ebx, ebx
-
+    xor edx, edx       ; Clear edx register, which will store the sum
+    xor ebx, ebx       ; Clear ebx register, which will be used as an index
 
     sum_loop:
-        add edx, [eax+ebx]
-        add ebx, 4
-        loop sum_loop
+        add edx, [eax+ebx] ; Add the value at the current index of the array to edx
+        add ebx, 4         ; Increment the index by 4 (since it's an array of integers)
+        loop sum_loop      ; Decrement ecx and repeat the loop if ecx is not zero
 
-    jmp exit
+    jmp exit              ; Jump to the exit label
 
 exit:
-    xor eax, eax
-    mov eax, edx
-    leave
-    ret
+    xor eax, eax          ; Clear eax register
+    mov eax, edx          ; Move the sum (stored in edx) into eax
+    leave                 ; Restore the old base pointer
+    ret                   ; Return, with the sum in eax
