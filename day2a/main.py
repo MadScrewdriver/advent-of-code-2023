a540f722023961bd6d6cfd58d1d14a10e31dab7b
Franciszek Lajszczak <f.lajszczak@gmail.com>
2023-12-02 22:38:14 +0100

    Day 2  added

---
+++ day2a/main.py
@@ -0,0 +1,1 @@
+print(sum(e*all(list(map(eval, l.strip().replace(" blue","<15").replace(" green","<14").replace(" red","<13").replace(", ", " and ").split(": ")[-1].split("; ")))) for e, l in enumerate(open("input.txt"), 1)))
---
+++ day2b/main.py
@@ -0,0 +1,1 @@
+print(sum(list(eval(str(tuple(dict(map(lambda x: (x[0], len(x)), sorted(map(eval, l.strip().replace(" blue","*'b'").replace(" green","*'g'").replace(" red","*'r'").replace(";", ",").split(": ")[-1].split(","))))).values())).replace(',','*')) for l in open("input.txt"))))
